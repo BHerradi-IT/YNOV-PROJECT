c3ad60afb9ab4651b20e573b39c8886634f6cff2
peace2019 <herradipeace@gmail.com>
2026-03-24 11:35:29 +0100

    added new project

--- clear_storage.js
+++
@@ -1,5 +0,0 @@
-// Script pour effacer le localStorage du navigateur
-console.log('Effacement du localStorage...');
-localStorage.clear();
-console.log('LocalStorage effacé!');
-alert('LocalStorage et SessionStorage effacés!');
--- security/firewall-rules.md
+++
@@ -1,7 +0,0 @@
-Allow: 80
-Allow: 443
-
-Block: all unused ports
-
-Enable WAF protection
-Enable DDoS protection
